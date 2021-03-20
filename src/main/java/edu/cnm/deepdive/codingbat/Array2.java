a51d5b9a0deee9a81eeda7c5613140685f66476b
rbrazell1 <brazellrussell@gmail.com>
2021-03-20 13:55:18 -0600

    countEvens finished

--- src/main/java/edu/cnm/deepdive/codingbat/Array2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Array2.java
@@ -3,6 +3,14 @@ package edu.cnm.deepdive.codingbat;
 public class Array2 {
 
   public int countEvens(int[] nums) {
+    int evenCount = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] % 2 == 0) {
+        evenCount++;
+      }
+      return evenCount;
+
+    }
     return 0; // TODO Compute & return value as specified in https://codingbat.com/prob/p162010.
   }
 
