a6e1d57c862c36aff4fb8c02984f7ccc8dea87cf
rbrazell1 <brazellrussell@gmail.com>
2021-03-20 16:00:07 -0600

    added black jack

--- src/main/java/edu/cnm/deepdive/codingbat/Logic2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Logic2.java
@@ -3,7 +3,17 @@ package edu.cnm.deepdive.codingbat;
 public class Logic2 {
 
   public int blackjack(int a, int b) {
-    return 0; // TODO Compute & return value as specified in https://codingbat.com/prob/p117019.
+    int winner = 0;
+    if (a <= 21 & a > b | b > 21) {
+        winner = a;
+        }
+    if (b <= 21 & b > a | a > 21) {
+      winner = b;
+      }
+    if (a > 21 & b > 21) {
+      winner = 0;
+    }
+    return winner;
   }
 
   public boolean makeBricks(int small, int big, int goal) {
