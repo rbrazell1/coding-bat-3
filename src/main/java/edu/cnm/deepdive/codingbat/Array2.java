c8742439e8b77d7e4b4dba77c1435b81e022d1d4
rbrazell1 <brazellrussell@gmail.com>
2021-03-21 16:01:26 -0600

    update to centeredAverage

--- src/main/java/edu/cnm/deepdive/codingbat/Array2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Array2.java
@@ -16,7 +16,6 @@ public class Array2 {
 
   public int centeredAverage(int[] numsC) {
     int mean = 0;
-    stream(numsC).sequential() -= numsC[0] - numsC[numsC.length + 1];
     for (int i = 0; i < numsC.length; i++) {
       mean += numsC[i];
     }
