bff9659db3bf927ea59d67f5b6ca899cfe593711
rbrazell1 <brazellrussell@gmail.com>
2021-03-21 15:57:05 -0600

    attempting sumMultiples

--- src/main/java/edu/cnm/deepdive/codingbat/Custom.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Custom.java
@@ -3,8 +3,8 @@ package edu.cnm.deepdive.codingbat;
 public class Custom {
 
   public int sumMultiples(int limitNum, int div1, int div2) {
-
-    return 0;
+    int halfSum = (limitNum / div1) +(limitNum / div2);
+    return halfSum;
   }
 
 }
