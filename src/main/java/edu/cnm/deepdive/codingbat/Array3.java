041f75d256de19abe895a62ec158ffb9abc3d83f
Russell Brazell <brazellrussell@gmail.com>
2021-04-13 20:40:59 -0600

    reviewed solution in office hours

--- src/main/java/edu/cnm/deepdive/codingbat/Array3.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Array3.java
@@ -3,22 +3,15 @@ package edu.cnm.deepdive.codingbat;
 public class Array3 {
 
   public boolean canBalance(int[] nums) {
-    boolean balanced = false;
-    int firstHalf;
-    int lastHalf;
-// I need two loops to start on the ends and work my way in checking if they are equal,
-// if they meet and are not equal then more to the side that is higher until they are equal,
-// if they go past equal then go back, if not equal then return false.
     for (int i = 0; i < nums.length; i++) {
-
+      int sum = 0;
+      for (int j = 0; j < i; j++)
+        sum += nums[j];
+      for (int j = i; j < nums.length; j++)
+        sum -= nums[j];
+      if (sum == 0) return true;
     }
-
-    if (firstHalf == lastHalf) {
-      balanced = true;
-    }
-
-    return balanced;
-
+    return false;
   }
 
 
