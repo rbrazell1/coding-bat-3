0c95094b3176499babb46667f5494327b55ff919
Russell Brazell <brazellrussell@gmail.com>
2021-04-15 01:13:45 -0600

    found one more solution

--- src/main/java/edu/cnm/deepdive/codingbat/Map2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Map2.java
@@ -1,17 +1,20 @@
 package edu.cnm.deepdive.codingbat;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Map2 {
 
   public Map<String, Integer> wordCount(String[] strings) {
-    int count = 0;
-    Map<String, Integer> map = new HashMap<String, Integer>(strings.length);
-    map.get(strings);
-    if (map.containsKey(strings)) {
-      return map.put(this, count++);
+    Map<String, Integer> map = new HashMap<>(strings.length);
+    for (int i = 0; i < strings.length; i++) {
+      String key = strings[i];
+      int value = map.getOrDefault(key, 0);
+      map.put(key, value++);
+      if (map.containsKey(key)) {
+        value++;
+        map.replace(key, value++);
+      }
     }
     return map;
   }
