c350f87646b8297bdb2d042cf53ace4916b1a28a
Russell Brazell <brazellrussell@gmail.com>
2021-05-19 22:36:47 -0600

    Finished AB2. works great

--- src/main/java/edu/cnm/deepdive/codingbat/Map2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Map2.java
@@ -48,6 +48,13 @@ public class Map2 {
     return map;
   }
 
-
+  public Map<String, String> mapAB2(Map<String, String> map) {
+    if (map.containsKey("a") && map.containsKey("b") &&
+        (map.get("a").equals(map.get("b")))) {
+      map.remove("a");
+      map.remove("b");
+    }
+    return map;
+  }
 
 }
