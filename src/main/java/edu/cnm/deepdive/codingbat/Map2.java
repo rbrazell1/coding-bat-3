4783391757b63a619250c002202494c405e803e8
Russell Brazell <brazellrussell@gmail.com>
2021-05-19 22:17:00 -0600

    got it on the first try

--- src/main/java/edu/cnm/deepdive/codingbat/Map2.java
+++ src/main/java/edu/cnm/deepdive/codingbat/Map2.java
@@ -42,8 +42,9 @@ public class Map2 {
   }
 
   public Map<String, String> mapAB(Map<String, String> map) {
-
-
+    if (map.containsKey("a") && map.containsKey("b")) {
+      map.put("ab", map.get("a") + map.get("b"));
+    }
     return map;
   }
 
